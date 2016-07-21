c53d0ba7e652f8285ba70add5910122067648ec0
Steve Ottenad <steveo@bluerooster.com>
2016-07-21 11:39:22 -0700

    More commits

--- helloworld/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ helloworld/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -10,9 +10,10 @@ import {
 import { DisplayMode } from '@ms/sp-client-base';
 import HelloWorldViewModel from './HelloWorldViewModel';
 import * as ko from 'knockout';
-
+import * as weather from 'weather-icons';
 import MockHttpClient from './tests/MockHttpClient';
 import strings from './loc/Strings.resx';
+
 
 export interface IHelloWorldWebPartProps {
   description: string;
